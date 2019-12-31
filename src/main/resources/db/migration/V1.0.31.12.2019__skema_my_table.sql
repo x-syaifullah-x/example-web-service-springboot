13163ca9d485748dffaf2e87cfc00363c07a5954
xxx <x.syaifullah.x@gmail.com>
2019-12-31 07:28:14 +0700

    ...

--- src/main/resources/db/migration/V1.0.31.12.2019__skema_my_table.sql
+++ src/main/resources/db/migration/V1.0.31.12.2019__skema_my_table.sql
@@ -1,15 +1,7 @@
-create table table_1
+create table data
 (
     ID    INT PRIMARY KEY AUTO_INCREMENT,
     NAME  VARCHAR(255) NOT NULL UNIQUE,
-    IMAGE BLOB         NOT NULL
+    IMAGE LONGBLOB     NOT NULL
 
 );
-
-# create table kelurahan_2
-# (
-#     id      INT PRIMARY KEY AUTO_INCREMENT,
-#     kode    VARCHAR(10)  NOT NULL UNIQUE,
-#     nama    VARCHAR(255) NOT NULL,
-#     kodepos VARCHAR(10)  NOT NULL
-# );
