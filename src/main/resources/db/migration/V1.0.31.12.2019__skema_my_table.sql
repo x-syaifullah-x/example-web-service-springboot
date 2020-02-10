1cc01566d7794fb8a72de2c0172ca32ea3e2691d
xxx <x.syaifullah.x@gmail.com>
2020-02-10 21:25:52 +0700

    ...

--- src/main/resources/db/migration/V1.0.31.12.2019__skema_my_table.sql
+++ src/main/resources/db/migration/V1.0.31.12.2019__skema_my_table.sql
@@ -3,3 +3,6 @@ create table data (
     NAME  VARCHAR(255) NOT NULL UNIQUE,
     IMAGE LONGBLOB     NOT NULL
 );
+
+-- create user database
+-- grant all on data.* to user@localhost identified by '19021992'
